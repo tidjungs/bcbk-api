4e5d91cbe17f439f7e33bd5145e61b80a34cc63b
tidjungs <waewprach.s@ku.th>
2017-10-13 15:12:33 +0700

    connect with mlab

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('../config/db');
 
 const app = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  
+  require('./routes')(app, database);
+  
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
