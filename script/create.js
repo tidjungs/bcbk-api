872dfbf0baac971a1c9e299ca42181a2fe3c2a02
tidjungs <waewprach.s@ku.th>
2017-10-16 21:15:01 +0700

    fix generate sessions , add sort time by start and room

--- script/create.js
+++ script/create.js
@@ -2,14 +2,14 @@ const request = require('request');
 const _ = require('lodash');
 
 const rooms = ["17201", "17302", "17303", "17304", "17401", "17402"];
-const starts = ["10:45", "11:15", "11:45"];
-const ends = ["11:10", "11:40", "12:10"];
+const starts = ["10:45", "11:15", "11:45", "13:00", "13:30", "14:00", "14:30", "15:20", "15:50", "16:20"];
+const ends = ["11:10", "11:40", "12:10", "13:25", "13:55", "14:25", "14:55", "15:45", "16:15", "16:45"];
 
 _.map(rooms, room => {
   _.map(starts, (start, index) => {
     const payload = {
-      name: "dump",
-      speaker: "dump",
+      name: "",
+      speaker: "",
       start: "2017-10-13 " + start,
       end: "2017-10-13 " + ends[index],
       room
--- src/admin/controller.js
+++ src/admin/controller.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const passport = require('passport');
+const _ = require('lodash');
 const isLoggedIn = require('../middleware').isLoggedIn;
 const Session = require('../session/model');
 const router = express.Router();
 
 router.get('/', isLoggedIn, function(req, res) {
   Session.find({}, function (err, sessions) {
+    sessions = _.sortBy(sessions, ['start', 'room']);
     res.render('index', { sessions });
   });
 });
